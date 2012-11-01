e8bad1368d1ca24a0c5169ee48f667a44d2f50ca
Attila Nagy <attilanagy85@gmail.com>
2012-11-01 14:13:30 +0100

    Fix in make file, all functionality added to client and server, testing ongoing.

--- make.sh
+++ make.sh
@@ -1,1 +1,21 @@
-python ~/tools/seattle/seattle_repy/repy.py src/restrictions.test src/reliable_client.repy 
+REPY_PATH=~/tools/seattle/seattle_repy/
+
+dir_ori=$(pwd)
+cd $(dirname $(pwd)/$0) || exit 1
+rm -r compile
+mkdir compile &&
+cp -rv src/* compile
+
+cd compile
+
+python ${REPY_PATH}/repypp.py reliable_server.repy server.repy
+python ${REPY_PATH}/repypp.py reliable_client.repy client.repy
+
+#cp client.repy server.repy ..
+
+echo To run client
+echo python ${REPY_PATH}/repy.py compile/restrictions.test compile/client.repy 
+echo To run server
+echo python ${REPY_PATH}/repy.py compile/restrictions.test compile/server.repy 
+
+exit
