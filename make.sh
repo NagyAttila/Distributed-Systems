dacad109fe204f37175c249b09b18bd28016e892
Attila Nagy <attilanagy85@gmail.com>
2012-11-11 17:42:43 +0100

    Correction in exception handling on client side.

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-REPY_PATH=~/tools/seattle/seattle_repy/
+REPY_PATH=~/tools/demokit/
 
 dir_ori=$(pwd)
 cd $(dirname $(pwd)/$0) || exit 1
@@ -11,11 +11,16 @@ cd compile
 python ${REPY_PATH}/repypp.py reliable_server.repy server.repy
 python ${REPY_PATH}/repypp.py reliable_client.repy client.repy
 
+cd ../tst
+python ${REPY_PATH}/repypp.py reliable.repy test_reliable.repy
+
 #cp client.repy server.repy ..
 
 echo To run client
 echo python ${REPY_PATH}/repy.py compile/restrictions.test compile/client.repy 
 echo To run server
 echo python ${REPY_PATH}/repy.py compile/restrictions.test compile/server.repy 
+echo To run api tests
+echo python ${REPY_PATH}/repy.py compile/restrictions.test tst/test_reliable.repy
 
 exit
