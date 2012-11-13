39f2c98677d117054ec4f378624044ef295f0600
lorcs1 <attilanagy85@gmail.com>
2012-11-13 18:05:21 +0100

    Number of transmitted bytes are now printed out.

--- tst/measurement1.sh
+++ tst/measurement1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 REPY_PATH=~/tools/demokit/
+CODE=$REPY_PATH/code
 
 function run_test
 {
@@ -7,25 +8,31 @@ function run_test
   rtries=$2
   dgram=$3
   (
+  # init
+  rm $CODE/test_out*
   echo loadkeys $user; 
   echo as $user;
   echo browse;
   echo on %1 reset;
   echo on %2 reset;
-  echo on %1 run code/server.repy $out_file $client_port
-  echo on %2 upload code/$in_file $in_file
-  echo on %2 run code/client.repy $in_file $server_ip $server_port $client_ip $client_port $dgram $rtries $tout;
+
+  # run
+  echo on %1 run $CODE/server.repy $out_file $client_port
+  echo on %2 upload $CODE/$in_file $in_file
+  echo on %2 run $CODE/client.repy $in_file $server_ip $server_port $client_ip $client_port $dgram $rtries $tout;
   echo on %1 show log;
   echo on %2 show log;
-  echo on %1 download $out_file code/$out_file
+  echo on %1 download $out_file $CODE/$out_file
   echo exit;
   )| python seash.py
 
+  # evaluate
   echo -e"\n"
-  if diff code/test_in code/test_out*; then
+  if diff $CODE/test_in $CODE/test_out*; then
     echo Success with: 'tout='$tout 'rtries='$rtries 'dgram='$dgram
   else
     echo Failure with: 'tout='$tout 'rtries='$rtries 'dgram='$dgram
+    echo Transmitted bytes: $(cat $CODE/$out_file* | wc -c)/$(cat $CODE/$in_file | wc -c)
   fi
 }
 
@@ -40,7 +47,9 @@ client_ip='203.159.127.3'
 client_port='63177'
 
 cd $REPY_PATH
-rm code/test_out*
 
-run_test 1000 4 512
+# to see only the results extend the commands with 
+# '| egrep "Success with|Failure with"'
+run_test 1000 0 512 
+run_test 1000 0 8192
 
